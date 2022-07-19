0f016d7450dcea146acc5cb152ef29a3950e2697
anaplopez2 <anaplopez07@gmail.com>
2022-07-19 01:13:59 -0300

    general update

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -21,11 +21,89 @@ productRouter.post('/', async (req, res) => {
 });
 
 productRouter.get('/lista-productos', async (req, res) => {
-  const response = await ProductApi.getAll();
+  try {
+    const products = await ProductApi.getAll();
+    res.send(products);
+  } catch (error) {
+    res.statusCode(404).send({ error: error });
+  }
+});
 
-  if (!response) res.send({ error: productNotFound });
+productRouter.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
 
-  res.send({ productos: response });
+    const product = await ProductApi.getById(id);
+
+    if (!product) {
+      throw { error: ERRORS_UTILS.MESSAGES.NO_PRODUCT };
+    }
+
+    res.send(product);
+  } catch (error) {
+    res.statusCode(400).send({ error: error });
+  }
+});
+
+productRouter.post('/nuevo-producto', isAdmin, async (req, res) => {
+  try {
+    const { nombre, descripcion, codigo, foto, precio, stock } = req.body;
+
+    const product = await JOI_VALIDATOR.product.validateAsync({
+      nombre,
+      descripcion,
+      codigo,
+      foto,
+      precio,
+      stock,
+    });
+
+    const productSaved = await ProductApi.save(product);
+
+    res.send(productSaved);
+  } catch (error) {
+    res.statusCode(400).send({ error: error });
+  }
+});
+
+productRouter.put('/:id', isAdmin, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { nombre, descripcion, codigo, foto, precio, stock } = req.body;
+
+    const product = await JOI_VALIDATOR.product.validateAsync({
+      nombre,
+      descripcion,
+      codigo,
+      foto,
+      precio,
+      stock,
+    });
+
+    const productUpdate = await ProductApi.updateById(id, product);
+
+    res.send(productUpdate);
+  } catch (error) {
+    res.statusCode(400).send({ error: error });
+  }
+});
+
+productRouter.delete('/:id', isAdmin, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await ProductApi.getById(id);
+    if (!product) {
+      throw { error: ERRORS_UTILS.MESSAGES.NO_PRODUCT };
+    }
+    const productDelete = await ProductApi.deleteById(id);
+
+    res.send({
+      mensaje: 'Producto eliminado',
+      productoEliminado: productDelete,
+    });
+  } catch (error) {
+    res.statusCode(404).send({ error: error });
+  }
 });
 
 export { productRouter };
